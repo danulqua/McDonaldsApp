314dd5fe94db94df4e419d39e96f4f4a805ec9ff
danil0110 <danilmail0110@gmail.com>
2020-11-21 21:16:30 +0200

    Minor fix to single offer page styles

--- js/views/offerPage.js
+++ js/views/offerPage.js
@@ -1,8 +1,8 @@
 const view = (db, offerId) => {
     let result = `
             <div class="mt-4 pt-4">
-                <h2 class="text-center text-dark font-weight-bold">${db.offers[offerId - 1].slogan}</h2>
-                <img class="w-100 mt-4 mb-4 rounded shadow" src="${db.offers[offerId - 1].img}" draggable="false" alt="${db.offers[offerId - 1].name}" />
+                <h2 class="text-center text-dark font-weight-bold mb-5">${db.offers[offerId - 1].slogan}</h2>
+                <img class="w-100 mb-4 rounded shadow" src="${db.offers[offerId - 1].img}" draggable="false" alt="${db.offers[offerId - 1].name}" />
                 <h2 class="text-center text-dark font-weight-bold mb-4">${db.offers[offerId - 1].name}</h2>
                 <p class="text-center">${db.offers[offerId - 1].description}</p>
             </div>
