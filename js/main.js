aac60f2d8a764ddf770f5a2a318387c8eb2166d8
danil0110 <danilmail0110@gmail.com>
2020-11-21 23:14:25 +0200

    Order pay markup done. Todo: view and validation

--- js/main.js
+++ js/main.js
@@ -7,38 +7,38 @@ let templateEngine = new TemplateEngine();
 let db;
 let view;
 
-window.onload = () => {
-    fetch("https://my-json-server.typicode.com/danil0110/McDonaldsDB/db")
-        .then(response => response.json())
-        .then(data => {
-            db = data;
-            let { viewName, categoryId, productId, offerId } = router.getCurrentPath(db);
-            import(`./views/${viewName}.js`)
-                .then(viewModule => {
-                    view = viewModule.default;
-                    renderPage(categoryId, productId, offerId);
-            })
-        });
+// window.onload = () => {
+//     fetch("https://my-json-server.typicode.com/danil0110/McDonaldsDB/db")
+//         .then(response => response.json())
+//         .then(data => {
+//             db = data;
+//             let { viewName, categoryId, productId, offerId } = router.getCurrentPath(db);
+//             import(`./views/${viewName}.js`)
+//                 .then(viewModule => {
+//                     view = viewModule.default;
+//                     renderPage(categoryId, productId, offerId);
+//             })
+//         });
 
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-}
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+// }
 
-window.onhashchange = () => {
-    let { viewName, categoryId, productId, offerId } = router.getCurrentPath(db);
-    import(`./views/${viewName}.js`)
-        .then(viewModule => {
-            view = viewModule.default;
-            renderPage(categoryId, productId, offerId);
-    });
+// window.onhashchange = () => {
+//     let { viewName, categoryId, productId, offerId } = router.getCurrentPath(db);
+//     import(`./views/${viewName}.js`)
+//         .then(viewModule => {
+//             view = viewModule.default;
+//             renderPage(categoryId, productId, offerId);
+//     });
 
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     });
+// }
 
 function renderPage(categoryId, productId, offerId) {
     if (categoryId != 0) {
